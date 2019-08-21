7bedfb0fee964e9b335b3e7794cfa72f815f304f
Jordan Ovrè <ovre.jordan@gmail.com>
2019-08-21 14:04:21 +0200

    Change name & url format

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 __author__ = ""
 
 description = ''
-name = 'hbfmodules.<module_name>'
+name = 'hbfmodules.<category>.<module_name>'
 setup(
     name=name,
     version='0.0.1',
@@ -13,7 +13,7 @@ setup(
     license='GPLv3',
     description=description,
     author=__author__,
-    url='https://github.com/hydrabus-framework/' + name + '.<module_name>',
+    url='https://github.com/hydrabus-framework/' + name,
     install_requires=[],
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
