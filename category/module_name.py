54f12da3bad41780b08c61dc980172fde3e810b2
Jordan Ovrè <ovre.jordan@gmail.com>
2019-08-21 15:57:09 +0200

    Add meta update to avoid forgetfulness

--- category/module_name.py
+++ category/module_name.py
@@ -4,6 +4,12 @@ from hydrabus_framework.modules.AModule import AModule
 class ClassName(AModule):
     def __init__(self):
         super(ClassName, self).__init__()
+        self.meta.update({
+            'name': '',
+            'version': '',
+            'description': '',
+            'author': ''
+        })
 
     def run(self):
         """
