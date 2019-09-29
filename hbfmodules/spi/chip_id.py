548508b3c9e9a765a3e152e9080d2ed5d0e6b41e
ghecko <ovre.jordan@gmail.com>
2019-09-29 19:58:42 +0200

    Use pyHydrabus instead of core functions

--- hbfmodules/spi/chip_id.py
+++ hbfmodules/spi/chip_id.py
@@ -2,8 +2,7 @@ import serial
 import codecs
 
 from hydrabus_framework.modules.AModule import AModule
-from hydrabus_framework.utils.hb_generic_cmd import hb_connect_bbio, hb_reset, hb_close
-from hydrabus_framework.utils.protocols.spi import hb_switch_spi, hb_configure_spi_port
+from hydrabus_framework.utils.pyHydrabus.spi import SPI
 
 
 __author__ = "Jordan Ovrè <ghecko78@gmail.com>"
@@ -18,7 +17,7 @@ class ClassName(AModule):
             'description': 'Module to recover SPI chip ID',
             'author': 'Jordan Ovrè <ghecko78@gmail.com>'
         })
-        self.serial = serial.Serial()
+        self.hb_serial = None
         self.options = [
             {"Name": "hydrabus", "Value": "", "Required": True, "Type": "string",
              "Description": "Hydrabus device", "Default": self.config["HYDRABUS"]["port"]},
@@ -32,48 +31,29 @@ class ClassName(AModule):
              "Description": "set SPI phase (high or low)", "Default": "low"}
         ]
 
-    def connect(self):
-        """
-        Connect to hydrabus and switch into BBIO mode
-        :return: Bool
-        """
-        try:
-            device = self.get_option_value("hydrabus")
-            self.serial = hb_connect_bbio(device=device, baudrate=115200, timeout=1)
-            if not self.serial:
-                raise UserWarning("Unable to connect to hydrabus device")
-            return True
-        except UserWarning as err:
-            self.logger.handle("{}".format(err), self.logger.ERROR)
-            return False
-
     def init_hydrabus(self):
         """
         Manage connection and init of the hydrabus into BBIO spi mode
         :return: Bool
         """
-        if self.connect():
-            if hb_switch_spi(self.serial):
-                return True
-            else:
-                self.logger.handle("Unable to switch hydrabus in spi mode, please reset it", self.logger.ERROR)
-                return False
-        else:
-            self.logger.handle("Unable to connect to hydrabus", self.logger.ERROR)
+        try:
+            device = self.get_option_value("hydrabus")
+            timeout = int(self.get_option_value("timeout"))
+            self.hb_serial = SPI(device)
+            self.hb_serial.timeout = timeout
+            self.hb_serial.polarity = self.get_option_value("spi_polarity")
+            self.hb_serial.phase = self.get_option_value("spi_phase")
+            self.hb_serial.device = self.get_option_value("spi_device")
+            return True
+        except serial.SerialException as err:
+            self.logger.handle("{}".format(err), self.logger.ERROR)
             return False
 
     def chip_id(self):
-        buf = bytearray()
         self.logger.handle("Sending RDID command...", self.logger.INFO)
-        # write-then-read: write one byte, read 3 (rdid)
-        self.serial.write(b'\x04\x00\x01\x00\x03')
-
-        # send rdid byte (0x9f)
-        self.serial.write(b'\x9f')
-
-        hb_ret = self.serial.read(1)
-        if b'\x01' == hb_ret:
-            buf = self.serial.read(3)
+        # write-then-read: write one byte, read 3 (rdid); \x9f = rdid command
+        buf = self.hb_serial.write_read(data=b'\x9f', read_len=3)
+        if buf is not None:
             cid = codecs.encode(buf, 'hex').decode().upper()
             manufacturer = cid[0:2]
             mem_type = cid[2:4]
@@ -89,12 +69,6 @@ class ClassName(AModule):
         :return: Nothing
         """
         if self.init_hydrabus():
-            result = hb_configure_spi_port(self.serial,
-                                           polarity=self.get_option_value("spi_polarity"),
-                                           phase=self.get_option_value("spi_phase"),
-                                           spi_device=self.get_option_value("spi_device"))
-            if result:
-                self.chip_id()
+            self.chip_id()
             self.logger.handle("Reset hydrabus to console mode", self.logger.INFO)
-            hb_reset(self.serial)
-            hb_close(self.serial)
+            self.hb_serial.close()
